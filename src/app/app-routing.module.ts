72153049be5288a9a9018650c7c5583038350625
zexhior <herizo@gmail.com>
2021-05-28 16:21:57 +0300

    ajout header et footer dans tous les components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ActiviteComponent } from './activite/activite.component';
 import { LoginComponent } from './login/login.component';
 import { ManagerComponent } from './manager/manager.component';
-import { HeaderComponent } from './header/header.component';
+import { MembreComponent } from './membre/membre.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'manager', component: ManagerComponent},
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'activite', component: ActiviteComponent},
+  {path: 'membre', component: MembreComponent}
 ];
 
 @NgModule({
